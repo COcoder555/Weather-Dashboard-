a79a3d2cc578bde05035947497d21fee2957dccb
COcoder555 <cocoder555@gmail.com>
2021-05-28 14:16:23 -0600

    Added buttons of search history

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 var Input = document.querySelector("#Input");
 var $Search = document.querySelector("#Search");
 var forcast = document.querySelectorAll(".card-title");
-sustain()""
+var data = JSON.parse(localStorage.getItem("cityName"))||[];
+var $history = document.querySelector("#history");
+// sustain();
 
 $Search.addEventListener("click", input)
-function Search(cityName) {
+function Search(cityName, event) {
  
   
   var urlApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + " &appid=05e10c92e64f28acd7117c4f4a378cef"
@@ -13,18 +15,21 @@ for(var i = 0; i <forcast.length; i++ ){
 }
   fetch(urlApi)
     .then(function (response) {
-      response.json().then(function (data) {
+      response.json().then(function (response) {
 
-        var latitude = data.coord.lat;
-        var longitude = data.coord.lon;
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
         var urlApi2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=05e10c92e64f28acd7117c4f4a378cef"
         fetch(urlApi2)
           .then(function (response) {
-            response.json().then(function (data) {
-              localStorage.setItem(cityName, JSON.stringify(data))
-              Display(cityName);
-              displayboard(cityName);
-
+            response.json().then(function (response) {
+              data.push(cityName);
+              localStorage.setItem("cityName", JSON.stringify(data))
+              Display(response);
+              displayboard(cityName,response);
+              if(!event){
+                history();
+              }
 
             })
           })
@@ -34,19 +39,18 @@ for(var i = 0; i <forcast.length; i++ ){
     });
 }
 function Display(cityName) {
-  var data = JSON.parse(localStorage.getItem(cityName));
-  console.log(JSON.parse(localStorage.getItem(cityName)));
+   console.log(cityName);
     for (var i = 0; i < forcast.length; i++) {
     var $date = document.createElement('h3');
     $date.textContent = moment().add((i + 1), 'days').format("MMMM Do YYYY");
     var temp = document.createElement("p");
-    temp.textContent = "Temp: " + data.daily[i].temp.day + "° F";
+    temp.textContent = "Temp: " + cityName.daily[i].temp.day + "° F";
     var hum = document.createElement("p");
-    hum.textContent= "Humidity: " + data.daily[i].humidity + "%";
+    hum.textContent= "Humidity: " + cityName.daily[i].humidity + "%";
     var wind= document.createElement("p");
-    wind.textContent= "Wind: "+ data.daily[i].wind_speed + " MPH";
+    wind.textContent= "Wind: "+ cityName.daily[i].wind_speed + " MPH";
     var icon= document.createElement("img");
-    icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+    icon.setAttribute("src", "http://openweathermap.org/img/wn/" + cityName.daily[i].weather[0].icon + "@2x.png");
     forcast[i].append($date, temp,hum,wind,icon,);
     
   };
@@ -58,22 +62,46 @@ function input(event) {
   Search(cityName);
   
 }
-function sustain(cityName) {Search("Denver")
+// function sustain(cityName) {Search("Denver")
+  
+// };
+
+function history() {
+  console.log(data);
+
+  
+  $history.innerHTML="";
+  for(var i = 0; i < data.length; i ++){
+  var historyButton = document.createElement("button");
+  historyButton.textContent = data[i];
+  $history.append(historyButton);
+  };
+  // historyButton(){
+  //   // Display();
+  // }
+};
+history();
+
+function historyClick(event){
+  
+  Search(event.target.textContent,event)
+  console.log(event.target.textContent);
   
 };
+$history.addEventListener("click",historyClick);
 
-function displayboard(cityName) {
-  var data = JSON.parse(localStorage.getItem(cityName));
-  console.log(JSON.parse(localStorage.getItem(cityName)));
+
+function displayboard(cityName,response) {
+
   var $cityDate = document.querySelector("#cityDate");
  $cityDate.textContent = cityName +" " + moment().format('MMMM Do YYYY');
  var $temp = document.querySelector("#TEMP");
- $temp.textContent = " "+ data.current.temp +"° F";
+ $temp.textContent = " "+ response.current.temp +"° F";
  var $wind = document.querySelector("#WIND");
- $wind.textContent = " " + data.current.wind_speed + " MPH";
+ $wind.textContent = " " + response.current.wind_speed + " MPH";
  var $HUMIDITY = document.querySelector("#HUMIDITY");
- $HUMIDITY.textContent= " "+ data.current.humidity + "%";
+ $HUMIDITY.textContent= " "+ response.current.humidity + "%";
  var $UV = document.querySelector("#UV");
- $UV.textContent = " " + data.current.uvi;
+ $UV.textContent = " " + response.current.uvi;
 };
 
