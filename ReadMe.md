c4a30c6caefa679a98346fe691aabfb81eac8155
COcoder555 <cocoder555@gmail.com>
2021-05-28 19:35:08 -0600

    Added info to Readme.md

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,38 @@
 # Weather Dashboard
-* 
+* Weather Dashboard gives the user  current, up to date, weather infromation as well as a five day forcast.
+
+## Construction of the Landing Page: HTML, style.css, script.js
+        
+### HTML:
+* I created a basic index.html file in which I created :
+    * boilerplat html
+    * a link to bootstrap using :"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" 
+    * a line to moment.js for the dates that would be needed be rendured in the script.js file. 
+    * a "container" to which in I created in bootstrap:
+    * *  a Search bar 
+    * *  5 cards for the five day forcast
+    * *  a large card for the current weather
+* I created a div for the buttons to create space for the buttons that would be generated from the Search history function.
+
+### style.css: 
+* I targted multiple classes in the HTML to change:
+ * * Background colors
+ * * Text size
+ * * Font size
+ * * Border styling
+ * * Font Weight
+ * * Margin
+ * * Height
+
+ ### script.js:
+ * I grabbed  ID's and Class elements and stored them in variables.
+ * I created a Search function 
+ * I  added a click function even listner to the Search button.
+ * I created a function to Display the information  for the 5 day forcast in the cards I created in the HTML using bootstrap.
+     * * Inside this function a loop was neccisary to get the information in sequential order according to the date. 
+
+ * I created an input function that stores the imput value of the user into variable called cityName and excutes the Search function.    
+ * I created a history button that crates a button based of the users search history. 
+ * I created a fucntion to display the current weather on the display board. 
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
+// Grabbing ID's and Class elements and soring them in variables.
+
 var Input = document.querySelector("#Input");
 var $Search = document.querySelector("#Search");
 var forcast = document.querySelectorAll(".card-title");
 var data = JSON.parse(localStorage.getItem("cityName"))||[];
 var $history = document.querySelector("#history");
 sustain();
-
+// Creating a Search function and adding a click function to the search button
 $Search.addEventListener("click", input)
 function Search(cityName, event) {
  
-  
+//  Doing the first fetch to aquare the  longitude and latidude that I will need to target specific cities in the second fetch I make.
   var urlApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + " &appid=05e10c92e64f28acd7117c4f4a378cef"
 for(var i = 0; i <forcast.length; i++ ){
   forcast[i].innerHTML= "";
 }
+//  Doing the second fetch to aquare the API data in need to populate the cards with weather information and store it in localstorage.
   fetch(urlApi)
     .then(function (response) {
       response.json().then(function (response) {
@@ -41,6 +44,8 @@ for(var i = 0; i <forcast.length; i++ ){
       )
     });
 }
+//  Created a function to Display the information  for the 5 day forcast in the cards I created in the HTML using bootstrap.
+//  Inside the function a loop was neccisary to get the information in sequential order according to the date. 
 function Display(cityName) {
    console.log(cityName);
     for (var i = 0; i < forcast.length; i++) {
@@ -59,6 +64,8 @@ function Display(cityName) {
   };
   
 };
+//  Created an input function that stores the imput value of the user into variable called cityName and excutes the Search function.
+
 function input(event) {
   event.preventDefault();
   var cityName = Input.value;
@@ -73,7 +80,7 @@ function sustain() {if(localStorage.getItem.cityName){
 };
   
 };
-
+// Created a history button that crates a button based of the users search history. 
 function history() {
   console.log(data);
 
@@ -96,7 +103,7 @@ function historyClick(event){
 };
 $history.addEventListener("click",historyClick);
 
-
+//  Created a fucntion to display the current weather on the display board. 
 function displayboard(cityName,response) {
 
  var $cityDate = document.querySelector("#cityDate");
